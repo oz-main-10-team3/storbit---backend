7780449346e16c06ea65b76ac1af40c1193b48dd
harry99990 <xornjs9502@naver.com>
2025-08-05 18:10:22 +0900

    apply code formatter

--- apps/studies/urls.py
+++ apps/studies/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from apps.studies.views import StudyDetailView
 
+app_name = "studies"
+
 urlpatterns = [
     path("studies/<int:id>/", StudyDetailView.as_view(), name="study-detail"),
 ]
