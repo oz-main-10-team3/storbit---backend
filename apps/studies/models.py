048b67e580e7cf1ac426f34f07c4a6943a636b0d
ryujaehak <jhryu627@gmail.com>
2025-08-06 18:30:30 +0900

    Application model 생성

--- apps/studies/models.py
+++ apps/studies/models.py
@@ -38,3 +38,28 @@ class Study(models.Model):
 
     def str(self):
         return self.title
+
+
+class StudyApplication(models.Model):
+    APPLICATION_STATUS_CHOICES = [
+        ('pending', '대기중'),
+        ('approved', '승인됨'),
+        ('rejected', '거절됨'),
+    ]
+
+    study = models.ForeignKey(Study, on_delete=models.CASCADE, related_name="applications")
+    applicant = models.ForeignKey(User, on_delete=models.CASCADE, related_name="applicants")
+    application_text = models.TextField(blank=True, null=True, help_text="자기소개 및 참여동기")
+    status = models.CharField(
+        max_length=20,
+        choices=APPLICATION_STATUS_CHOICES,
+        default='pending'
+    )
+
+    class Meta:
+        unique_together = ("study", "applicant") # 사용자가 한 스터디에 중복 신청 X
+        verbose_name = "스터디 신청 "
+        verbose_name_plural = "스터디 신청 목록"
+
+    def __str__(self) -> str:
+        return f"{self.applicant.nickname}의 {self.study.title} 신청 ({self.status})"
