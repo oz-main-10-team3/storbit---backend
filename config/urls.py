dcfc9c201b78045d28edbde75917532100686d5c
ryujaehak <jhryu627@gmail.com>
2025-08-13 11:10:44 +0900

    💡 chore: 병합충돌해결

--- config/urls.py
+++ config/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path("api/auth/", include("apps.users.urls")),
     path("api/", include("apps.events.urls")),
     path("api/", include("apps.studies.urls")),
+    path("api/auth/", include("apps.users.urls")),
 ]
